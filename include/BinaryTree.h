2a3902be04f7f9942b178b950d61d8c2a296b12a
Matheus2237 <mathpaulinor@gmail.com>
2023-11-10 20:07:58 -0300

    docs: Inclusão da documentação de alguns métodos

--- include/BinaryTree.h
+++ include/BinaryTree.h
@@ -15,16 +15,35 @@
 #define BINARY_TREE_H
 #include <string>
 
+// TODO: Documentar
 class BinaryTree {
     
     private:
+
+        // TODO: Documentar
         struct TreeNode {
+
             std::string entry;
             TreeNode* leftSubTree;
             TreeNode* rightSubTree;
 
+            /**
+             * @brief Constrói uma nova struct do tipo TreeNode.
+             * 
+             * @param nodeValue Valor de chave do nó.
+             */
             TreeNode(std::string nodeValue);
+            
+            /**
+             * @brief Aloca um TreeNode e verifica se a alocação ocorreu com sucesso.
+             *  Caso ocorra falha na alocação, o programa é abortado.
+             * 
+             * @param nodeValue Valor de chave do nó a ser alocado.
+             * @return TreeNode* Ponteiro para o nó alocado.
+             */
             static TreeNode* createNode(std::string nodeValue);
+            
+            // TODO: Documentar
             void setSubsequentSubTrees(std::string leftSubTree, std::string rightSubTree);
         };
         typedef TreeNode* TreePointer;
@@ -32,6 +51,10 @@ class BinaryTree {
         
     public:
         
+        /**
+         * @brief Destrói o objeto BinaryTree.
+         * 
+         */
         ~BinaryTree();
 
         static BinaryTree* readTreeFromFile(std::string&& filepath);
